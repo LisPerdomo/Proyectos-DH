da0e54bca585bd3c9c93cfa18bcfadbb6fae08be
Juan Ezcurra <jezcurra@ad.grupobiotoscana.com>
2021-12-22 21:21:29 -0300

    Modicación segunda vuelta

--- Testeando1.js
+++ Testeando1.js
@@ -1,2 +1,5 @@
 let inciandoGit = "Hola Sr. GIT";
 let modificacion = "¡¡¡Esta es la modificación efectuada en el file!!!"
+
+let variable = "Punto 7 del ejercicio, modificando ambos participantes al mismo tiempo"
+let variable2 = "Rellenando texto"
